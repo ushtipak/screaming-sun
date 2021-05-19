db1accb914a6526a09e2662439be892a8cff22c4
Milos Knezevic <ushtipak@gmail.com>
2021-05-19 18:52:19 +0200

    Add buttons for vibration test :)

--- app/src/main/java/com/pijupiju/screamingsun/MainActivity.java
+++ app/src/main/java/com/pijupiju/screamingsun/MainActivity.java
@@ -1,14 +1,45 @@
 package com.pijupiju.screamingsun;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar rightNow = Calendar.getInstance();
+        int hour = rightNow.get(Calendar.HOUR);
+        Log.d(TAG, "current hour: " + hour);
+
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+
+        btn1.setOnClickListener(view -> vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE)));
+        btn2.setOnClickListener(view -> vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE)));
+        btn3.setOnClickListener(view -> vibrator.vibrate(VibrationEffect.createOneShot(300, VibrationEffect.DEFAULT_AMPLITUDE)));
+        btn4.setOnClickListener(view -> vibrator.vibrate(VibrationEffect.createOneShot(400, VibrationEffect.DEFAULT_AMPLITUDE)));
+        btn5.setOnClickListener(view -> vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)));
     }
 }
